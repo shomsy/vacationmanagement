8eb08116c85b41ba0a34e82dbd3cbe3028f7e0bb
Milos Stankovic <solimcivoknats@gmail.com>
2024-06-13 15:56:13 +0200

    Models and Controllers. vacationmanagement migration

--- database/migrations/2024_06_13_011121_create_vacation_managements_table.php
+++ database/migrations/2024_06_13_011121_create_vacation_managements_table.php
@@ -4,15 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up() : void
     {
         Schema::create('vacation_managements', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('team_id');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->enum('status', ['pending', 'approved', 'rejected']);
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('team_id')->references('id')->on('teams');
+
             $table->timestamps();
         });
     }
@@ -20,7 +29,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down() : void
     {
         Schema::dropIfExists('vacation_managements');
     }
